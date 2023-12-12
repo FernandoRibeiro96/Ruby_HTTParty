36f1e33f8d55fda06182b283f284d98529aa2691
FernandoRibeiro96 <lluppribeiro1996@gmail.com>
2023-12-11 21:49:47 -0300

    feat: Criando logica de testes para os steps

--- features/step_definitions/get_info_step.rb
+++ features/step_definitions/get_info_step.rb
@@ -2,11 +2,23 @@ Given('for the user to consult employee information') do
     @get_url = 'http://dummy.restapiexample.com/api/v1/employees'
   end
   
-  When('he carry out the research') do
-    @list_employee = HTTParty.get(@get_url)         
-  end
+When('he carry out the research') do
+    @list_employee = HTTParty.get(@get_url)        
+end
   
-  Then('a list of employees must return') do
+Then('a list of employees must return') do
     expect(@list_employee.code).to eql 200
-    expect(@list_employee.message).to eql 'OK'       
-  end
+    expect(@list_employee.message).to eql 'OK' 
+    
+    parsed_employee = JSON.parse(@list_employee.body)
+    employee_id_to_find = 3
+    employee = parsed_employee['data'].find {|emp| emp['id'] == employee_id_to_find}
+
+    if employee
+        puts "Nome do funcionário: #{employee['employee_name']}"
+        puts "Salário do funcionário: #{employee['employee_salary']}"
+        puts "Idade do funcionário: #{employee['employee_age']}"
+    else
+        puts 'Funcionário não encontrado'
+    end
+end
